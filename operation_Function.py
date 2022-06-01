48be316c72b1d620ef0df1d039b2db13ea20168d
LIRUIQI12 <li995186428@gmail.com>
2022-06-01 22:39:56 +0300

    function plus

--- operation_Function.py
+++ operation_Function.py
@@ -1,3 +1,5 @@
+import math
+
 from intvalpy import Interval
 from twin import *
 from operation import *
@@ -12,10 +14,10 @@ def gamma(D1: twin_Function, D2: twin_Function):
             (D1.inside.b + D2.external.a) >= (D2.external.b + D1.inside.a):
         return D1.alpha
     elif D2.alpha != '0' and D2.inside_width() != -1 and D1.alpha != D2.alpha and \
-            (D1.external.b + D2.inside.a) >= (D2.inside.b + D1.external.a):
+            (D1.external.b + D2.inside.a) <= (D2.inside.b + D1.external.a):
         return D2.alpha
     else:
-        return 0
+        return '0'
 
 
 # D_1  = (I_1l,I_1,α)= (T_1,α)=([a-,a+ ],[А-,А+ ],α),
@@ -30,7 +32,7 @@ def plus_function(D1: twin_Function, D2: twin_Function):
             D1.inside_width() != -1 and D2.inside_width() != -1:
         if gamma(D1, D2) == '0':
             return twin_Function(Interval(None, None),
-                                 Interval(min(D1.external.a + D2.inside.b),
+                                 Interval(min(D1.external.a + D2.inside.b,D2.external.a+D1.inside.b),
                                           max(D1.inside.a + D2.external.b, D2.inside.a + D1.external.b)), '0').print_result()
         elif gamma(D1, D2) == D1.alpha:
 
@@ -45,3 +47,22 @@ def plus_function(D1: twin_Function, D2: twin_Function):
         T1=twin(D1.inside,D1.external)
         T2=twin(D2.inside,D2.external)
         return (plus(T1,T2).Print_result(), print('gamma',gamma(D1, D2)))
+
+def unary_minus_twin_function(D: twin_Function):
+    if D.alpha == '-':
+        return twin_Function(-D.inside,-D.external,'+')
+    if D.alpha == '+':
+        return twin_Function(-D.inside,-D.external,'-')
+    else:
+        return twin_Function(-D.inside, -D.external,'-')
+
+def inverse_twin_function(D: twin_Function):
+    if 0 in D.inside or 0 in D.external:
+        print("Cannot be divided into intervals containing 0.")
+        exit()
+    if D.alpha == '-':
+        return twin_Function(1/D.inside,1/D.external,'+')
+    if D.alpha == '+':
+        return twin_Function(1/D.inside,1/D.external,'-')
+    else:
+        return twin_Function(1/D.inside, 1/D.external,'-')
